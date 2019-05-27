8476082dea29143b8b3e19db971f8a691e92c8c4
censuradho <gustavoleiteoliveira800@gmail.com>
2019-05-27 13:51:47 -0300

    cor no campo de formularios

--- javascript/modal.js
+++
@@ -1,7 +0,0 @@
-const modal = document.querySelector('.modal-container');
-const closeModal = document.querySelector('.closeModal')
-
-function modalOff() {
-    modal.className = '.closeModal'
-}
-modal.addEventListener('click', modalOff)
